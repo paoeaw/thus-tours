e6480a4acd093c5f7fb7eb0511ed15f474bbf510
Alexander Moskowitz <42726237+ajemoskowitz@users.noreply.github.com>
2019-04-01 18:23:34 +0900

    Update booking.rb

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :customer, class_name: 'User', foreign_key: 'customer_id'
   belongs_to :tour
-  validates :headcount, presence: true
+  validates :headcount, presence: true, numericality: { only_integer: true }
   validates :date, presence: true
 end
