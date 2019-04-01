48587e79bd35b235e72476d832e742273f9743cf
Alexander Moskowitz <alexjmoskowitz@gmail.com>
2019-04-01 15:23:39 +0900

    improved connections between booking, tour, and users models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,4 @@
 class Booking < ApplicationRecord
   belongs_to :customer, class_name: 'User', foreign_key: 'customer_id'
   belongs_to :tour
-  belongs_to :provider, through: :tour
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  has_many :tours
-  has_many :bookings
+  has_many :tours, class_name: 'Tour', foreign_key: 'provider_id'
+  has_many :bookings, class_name: 'Booking', foreign_key: 'customer_id'
 end
