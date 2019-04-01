b96338456bab2e30d6c8bde38554cd951ef6773d
Alvin Lee <ALEE9@PROTONMAIL.COM>
2019-04-01 17:40:42 +0900

    Created validations for booking, tour, and user models.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :customer, class_name: 'User', foreign_key: 'customer_id'
   belongs_to :tour
+  validates :headcount, presence: true
+  validates :date, presence: true
 end
