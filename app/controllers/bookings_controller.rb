97be49be4f52f35ccae46305a828dbe9f5c48a1f
Alvin Lee <ALEE9@PROTONMAIL.COM>
2019-04-02 14:09:34 +0900

    Completed BookingsController

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,56 @@
 class BookingsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
+  def index
+    @bookings = policy_scope(Booking).order(created_at: :desc)
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+    authorize @booking
+  end
+
+  def new
+    @booking = Booking.new
+    authorize @booking
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    authorize @booking
+    @booking.update(tour_params)
+
+    redirect_to booking_path(@booking)
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    authorize @booking
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+    authorize @booking
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    authorize @booking
+    @booking.destroy
+
+    redirect_to bookings_path
+  end
+
+  private
+
+  def booking_params
+    ### left it at name and photo for testing purposes
+    ### include all params when ready
+    params.require(:booking).permit(:headcount, :date)
+  end
 end
