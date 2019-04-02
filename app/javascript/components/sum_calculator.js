845eec2d8374312c9b8cfa7a077ec5982f33e555
Alexander Moskowitz <alexjmoskowitz@gmail.com>
2019-04-02 22:44:59 +0900

    improved headcount input field

--- app/javascript/components/sum_calculator.js
+++ app/javascript/components/sum_calculator.js
@@ -1,12 +1,14 @@
+// calculates the total cost of a tour in realtime
+
 const calculateTotalCost = () => {
-  const headCount = document.querySelector('.booking_headcount')
+  const headCount = document.querySelector('#booking_headcount')
   let string_cost = document.querySelector("#tour_cost")
   const num_cost = parseInt(string_cost.innerText)
 
 
   if (headCount) {
     headCount.addEventListener('change',(event) => {
-      const string_count = headCount.childNodes[1].value
+      const string_count = headCount.value
       const num_count = parseInt(string_count, 10)
       string_cost.innerText = num_cost * num_count
     })
