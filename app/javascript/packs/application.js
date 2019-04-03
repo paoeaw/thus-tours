0cc6dda646be12c081938af18dff380c5437fec8
Alexander Moskowitz <alexjmoskowitz@gmail.com>
2019-04-03 15:43:11 +0900

    formatting, uncommented css link application.html.erb

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,18 +1,14 @@
 import "bootstrap";
 
 import 'mapbox-gl/dist/mapbox-gl.css';
-
-import { initMapbox } from '../plugins/init_mapbox';
-
-initMapbox();
-
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
+import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
-
-initAutocomplete();
-
 import { calculateTotalCost } from '../components/sum_calculator';
+
+initMapbox();
+initAutocomplete();
 calculateTotalCost();
 
 
